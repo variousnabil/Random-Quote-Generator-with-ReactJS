24ae391f6cd643527d9b0cd02179590c44c6c5d8
variousnabil <varionabilr@gmail.com>
2020-07-01 16:27:35 +0700

    Updates

---
+++ precache-manifest.65ac3ed20aaaee38d3a8c8a70fe22118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da07b4f400f59b5f6e0cfa557a6ca40",
+    "url": "/Random-Quote-Generator-with-ReactJS/index.html"
+  },
+  {
+    "revision": "30ee0364fb1c8f3f5499",
+    "url": "/Random-Quote-Generator-with-ReactJS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6caa89f9555102927601",
+    "url": "/Random-Quote-Generator-with-ReactJS/static/js/2.15975a23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Generator-with-ReactJS/static/js/2.15975a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ee0364fb1c8f3f5499",
+    "url": "/Random-Quote-Generator-with-ReactJS/static/js/main.16bd83c4.chunk.js"
+  },
+  {
+    "revision": "8402b49c5d555fd6e0bb",
+    "url": "/Random-Quote-Generator-with-ReactJS/static/js/runtime-main.efadbce6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Random-Quote-Generator-with-ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
