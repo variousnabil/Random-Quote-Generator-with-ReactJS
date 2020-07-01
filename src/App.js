31ada758ea1e5cf3fd5f9bc0dd08f6f8e240f63c
variousnabil <varionabilr@gmail.com>
2020-07-01 16:49:03 +0700

    update first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: 'Whatever you can do, or dream you can, begin it. Boldness has genius, power and magic in it.',
+      author: 'Johann Wolfgang von Goethe'
+    };
+  }
+  componentDidMount() {
+    const getRandomQuote = fetch('https://api.quotable.io/random');
+    getRandomQuote.then(res => res.json())
+      .then(res => {
+        this.setState({
+          quote: res.content,
+          author: res.author
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleClick = () => {
+    const getRandomQuote = fetch('https://api.quotable.io/random');
+    getRandomQuote.then(res => res.json())
+      .then(res => {
+        this.setState({
+          quote: res.content,
+          author: res.author
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <div id="text">{this.state.quote}</div>
+        <div id="author">- {this.state.author}</div>
+        <div id="footer">
+          <a href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${this.state.quote}" ${this.state.author}`} id="tweet-quote"><i className="fa fa-twitter fa-lg"></i></a>
+          <input type="button" onClick={this.handleClick} id="new-quote" value="New quote" />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
